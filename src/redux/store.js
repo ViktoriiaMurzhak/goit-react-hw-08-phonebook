fda5cb92ab22f5720937cb122400292f5d346651
Viktoriia <viktoria.murzhak@gmail.com>
2022-11-04 01:46:49 +0200

    fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -29,7 +29,7 @@ function contactsReducer(state = initialState, action) {
       return {
         ...state,
         contacts: state.contacts.filter(
-          contact => contact.id != action.payload
+          contact => contact.id !== action.payload
         ),
       };
 
